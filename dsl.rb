32cb77ccefb476b9241afa19f8c1ff45e55052a0
ongaeshi <ongaeshi0621@gmail.com>
2017-03-05 20:58:02 +0900

    Add show function

--- dsl.rb
+++ dsl.rb
@@ -3,8 +3,18 @@ require "hatebu/hatebu"
 
 module Hatebu
   module DSL
-    def category(name, &block)
-      block.call Hatebu.new(name).items
+    def category(name)
+      Hatebu.new(name).items
+    end
+
+    def show(data)
+      if data.is_a? Array
+        data.each { |e| show e }
+      else
+        print data.l_astr, " ", data.b_astr, "\n"
+        puts data.description
+        puts
+      end
     end
   end
 end
